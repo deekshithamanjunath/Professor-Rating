838de62b8e8da64892b445689d073bb9e1a290e4
Deekshitha Manjunath <deekshithamanjunath@Cordilia.local>
2016-11-12 03:44:59 +0530

    Initial Commit

---
+++ assignment5/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  assignment5
+//
+//  Created by Deekshitha Manjunath on 11/12/16.
+//  Copyright © 2016 Deekshitha Manjunath. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
